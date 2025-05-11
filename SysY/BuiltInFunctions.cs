70b455e92e8d5344da2666dbd82f67a8b1df9e37
ANNNgellalala <ANNNgel1619@outlook.com>
2025-05-11 10:38:22 +0800

    根据运行时内置函数生成预定义符号表

---
+++ SysY/BuiltInFunctions.cs
@@ -0,0 +1,38 @@
+﻿namespace SysY;
+
+public static class BuiltInFunctions
+{
+    public static List<string> BuiltInFunctionNames { get; } = new()
+    {
+        "getint",
+        "getch",
+        "getfloat",
+        "getarray",
+        "getfarray",
+        "putint",
+        "putch",
+        "putfloat",
+        "putarray",
+        "putfarray",
+        "putf", 
+        "starttime",
+        "stoptime",
+    };
+
+    public static Dictionary<string, Entry> BuiltInEntries { get; } = new Dictionary<string, Entry>
+        {
+            {"getint", Entry.CreateFunction(SysYType.Int, [])}, 
+            {"getch", Entry.CreateFunction(SysYType.Int, [])},
+            {"getfloat", Entry.CreateFunction(SysYType.Float, [])}, 
+            {"getarray", Entry.CreateFunction(SysYType.Int, [Entry.CreateArray(SysYType.Int, [])])},
+            {"getfarray", Entry.CreateFunction(SysYType.Float, [Entry.CreateArray(SysYType.Float, [])])},
+            {"putint", Entry.CreateFunction(SysYType.Void, [Entry.CreateArray(SysYType.Int, [])])},
+            {"putch", Entry.CreateFunction(SysYType.Void, [Entry.CreateArray(SysYType.Int, [])])},
+            {"putfloat", Entry.CreateFunction(SysYType.Void, [Entry.CreateArray(SysYType.Float, [])])},
+            {"putarray", Entry.CreateFunction(SysYType.Void, [Entry.CreateVariable(SysYType.Int), Entry.CreateArray(SysYType.Int, []),])},
+            {"putfarray", Entry.CreateFunction(SysYType.Void, [Entry.CreateVariable(SysYType.Int), Entry.CreateArray(SysYType.Float, []),])},
+            {"putf", Entry.CreateFunction(SysYType.Void, [])},
+            {"starttime", Entry.CreateFunction(SysYType.Void, [])},
+            {"stoptime", Entry.CreateFunction(SysYType.Void, [])},
+        };
+}
