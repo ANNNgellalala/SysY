1a0e93317c3fe143aa629f7ff66b5bb510c61abc
ANNNgellalala <ANNNgel1619@outlook.com>
2025-05-28 00:54:06 +0800

    增加单元测试，创建静态语义检查对应的几个样例

---
+++ SysY.Test/ExceptionErrorReporter.cs
@@ -0,0 +1,15 @@
+﻿using SysY.Reporter;
+
+namespace SysY.Test;
+
+public class ExceptionErrorReporter : IErrorReporter
+{
+    public void ReportError(
+        ErrorInfo error)
+    {
+        throw new ReportErrorException(error);
+    }
+}
+
+public class ReportErrorException(ErrorInfo errorInfo)
+    : Exception($"Error: {errorInfo.ErrorType} at {errorInfo.Start.Line}:{errorInfo.Start.Column} - {errorInfo.Stop.Line}:{errorInfo.Stop.Column}");
---
+++ SysY.Test/Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace SysY.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    [Description("对非数组变量采用下标变量的形式访问")]
+    public void VisitVariableErrorTest()
+    {
+        
+        Assert.Pass();
+    }
+}
